2ed59677660138d25cc89b94dba0c46f34188a69
unknown <ijon@stuy.edu>
2019-01-31 18:29:18 -0500

    hw0

--- maker.py
+++ maker.py
@@ -8,14 +8,24 @@ max = 255
 out = "foo.ppm"
 
 def header(f):
-    writeFile = open(f, 'w')
     writeFile.write(identifier + " ")
-    writeFile.write(rows + " ")
-    writeFile.write(cols + " ")
-    writeFile.write(max + "\n")
+    writeFile.write(str(rows) + " ")
+    writeFile.write(str(cols) + " ")
+    writeFile.write(str(max) + "\n")
 
 def body(f):
-    writeFile = open(f, 'w')
     for i in range(0, rows):
         for i in range(0, cols):
-            r = random.randint()
+            r = random.randint(0, max)
+            g = random.randint(0, max)
+            b = random.randint(0, max)
+            writeFile.write(str(r) + " ")
+            writeFile.write(str(g) + " ")
+            writeFile.write(str(b) + "\t")
+        writeFile.write("\n")
+
+def main():
+    writeFile = open(f, 'w')
+    header(out)
+    body(out)
+    writeFile.close()
